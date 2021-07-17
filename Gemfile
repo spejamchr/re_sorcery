dd6101364cce61846ab4deeb53f0980642cdbace
Spencer Christiansen <jc.spencer92@gmail.com>
2021-07-17 13:49:34 -0600

    Initial commit: 'bundle gem linked_payload'

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in linked_payload.gemspec
+gemspec
---
+++ lib/linked_payload.rb
@@ -0,0 +1,6 @@
+require "linked_payload/version"
+
+module LinkedPayload
+  class Error < StandardError; end
+  # Your code goes here...
+end
